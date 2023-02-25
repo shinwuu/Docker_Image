dae92a60c48e691cdbd5e4be9c7aee99d270bb97
shinwuu <eunjingshun@hotmail.com>
2023-02-25 06:47:19 -0500

    testing jenkins and Docker

--- app/app.py
+++ app/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return 'Hell DevSecOps!'
+    #return 'Hello, World!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
